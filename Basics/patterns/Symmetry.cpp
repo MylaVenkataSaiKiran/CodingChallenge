76cb5e378adb592ac1fe99297fa1bed8f9af9dbb
MylaVenkataSaiKiran <93196950+MylaVenkataSaiKiran@users.noreply.github.com>
2023-10-21 14:42:24 +0530

    symmetry

---
+++ Basics/patterns/Symmetry.cpp
@@ -0,0 +1,20 @@
+void symmetry(int n) {
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++) cout<<"* ";
+        for(int j=1;j<=2*(n-i);j++) cout<<" ";
+        for(int j=1;j<=i;j++) cout<<"* ";
+        cout<<endl;
+    }
+    for(int i=n-1;i>0;i--){
+        for(int j=1;j<=i;j++) cout<<"* ";
+        for(int j=1;j<=2*(n-i);j++) cout<<" ";
+        for(int j=1;j<=i;j++) cout<<"* ";
+        cout<<endl;
+    }
+}
+//pattern::
+// *         *
+// * *     * *
+// * * * * * *
+// * *     * *
+// *         *
