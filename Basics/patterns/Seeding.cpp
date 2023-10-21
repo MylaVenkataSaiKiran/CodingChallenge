f7e2036bf4178dc36ffc9808d579127914954b6a
MylaVenkataSaiKiran <93196950+MylaVenkataSaiKiran@users.noreply.github.com>
2023-10-21 11:38:28 +0530

    seeditng

---
+++ Basics/patterns/Seeding.cpp
@@ -0,0 +1,14 @@
+void seeding(int n) {
+	// Write your code here.
+	for(int i=n;i>0;i--){
+		for(int j=i;j>0;j--){
+			cout << "* ";
+		}
+		cout <<endl;
+	}
+}
+//pattern::
+//* * * *
+//* * *
+//* *
+//*
