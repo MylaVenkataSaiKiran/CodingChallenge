6042f1659e91d4ca55ae4e3d6c15453c4c2f6a3e
MylaVenkataSaiKiran <93196950+MylaVenkataSaiKiran@users.noreply.github.com>
2023-10-21 11:41:19 +0530

    Reverse Number Triangle

---
+++ Basics/patterns/Reverse Number Triangle.cpp
@@ -0,0 +1,14 @@
+void nNumberTriangle(int n) {
+    // Write your code here.
+    for(int i=n;i>0;i--){
+        for(int j=1;j<=i;j++){
+            cout <<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+//pattern::
+//1 2 3 4
+//1 2 3
+//1 2
+//1
