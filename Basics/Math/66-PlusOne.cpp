edcc7977f252d6020effc1357edce84b1e383d00
MylaVenkataSaiKiran <93196950+MylaVenkataSaiKiran@users.noreply.github.com>
2023-10-21 09:45:40 +0530

    plusone

--- Basics/Math/66-PlusOne.cpp
+++ Basics/Math/66-PlusOne.cpp
@@ -3,23 +3,15 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
 
-        int j=n-1;
-        while(j>=0){
-            if(digits[j]==9){ // if the digit is 9, then change it to 0
-                digits[j]=0;
-                if(j==0){ // if the first digit is 9, then we need to add a new digit
-                    digits.insert(digits.begin(),1);
-                    return digits;
-                }
-                else{ // if the digit is not the first digit, then go to the previous digit
-                    j--;
-                }
-            }
-            else{ // if the digit is not 9, then add 1 to it
+        for(int j=n-1;j>=0;j--){
+            if(digits[j]<9){
                 digits[j]++;
                 return digits;
             }
+            digits[j]=0;
         }
+        //if every digit is 9 it will come here
+        digits.insert(digits.begin(),1);
         return digits;
     }
 };
