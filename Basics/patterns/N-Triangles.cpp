015faab0f03e51b9063e4662045dba7da6aa597b
MylaVenkataSaiKiran <93196950+MylaVenkataSaiKiran@users.noreply.github.com>
2023-10-21 11:34:01 +0530

    nTriangle

---
+++ Basics/patterns/N-Triangles.cpp
@@ -0,0 +1,14 @@
+void nTriangle(int n) {
+	// Write your code here
+	for(int i=0;i<n;i++){
+		for(int j=0;j<=i;j++){
+			cout <<j+1<<" ";
+		}
+		cout << endl;
+	}
+}
+//pattern::
+//1
+//1 2
+//1 2 3
+//1 2 3 4
