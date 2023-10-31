4467ed2b14e36571c6d18a1696f4bf2b8fc8517d
MylaVenkataSaiKiran <93196950+MylaVenkataSaiKiran@users.noreply.github.com>
2023-10-31 15:58:44 +0530

    Longest Subarray With Sum K

---
+++ Arrays/Easy/Longest Subarray With Sum K.cpp
@@ -0,0 +1,48 @@
+//OPTIMAL solution using two pointers
+int longestSubarrayWithSumK(vector<int> a, long long k){
+    int right=0, left=0;
+    long long sum=0;
+    int maxlen = 0;
+    while(right < a.size()){
+        sum += a[right]; //add current elem to sum
+
+        //whenever sum > k remove some elem form back using left pointer
+        while(left<=right && sum>k){ 
+            sum -= a[left];
+            left++;
+        }
+
+        if(sum==k) maxlen = max(maxlen, right-left+1);
+
+        right++;
+    }
+
+    return maxlen;
+}
+
+
+
+//BETTER Solution using Hashing
+int longestSubarrayWithSumK(vector<int> a, long long k){
+    int maxlen = 0;
+    long long prefixsum = 0;
+    map<long long, int> m;
+
+    for(int i=0;i<a.size();i++){
+        prefixsum += a[i]; //cummulative sum
+        if(prefixsum==k) maxlen = max(maxlen, i+1);
+
+        //remaining subarray sum is prefixsum-k
+        //find index where this sum occurs and subtract it from
+        //current index this gives length
+        if(m.find(prefixsum-k)!=m.end()){ 
+            maxlen = max(i-m[prefixsum]+1, maxlen);
+        }
+        
+        if(m.find(prefixsum)==m.end()){ //prefix sum is not present in map
+            m.insert({prefixsum,i});
+        }
+    }
+
+    return maxlen;
+}
