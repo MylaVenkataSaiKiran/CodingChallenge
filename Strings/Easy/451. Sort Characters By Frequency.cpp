37ab0f0bd89d6eed1ecc0bc8128fb78da071d038
MylaVenkataSaiKiran <93196950+MylaVenkataSaiKiran@users.noreply.github.com>
2023-11-27 20:36:06 +0530

    sort chars by frequency1

--- Strings/Easy/451. Sort Characters By Frequency.cpp
+++ Strings/Easy/451. Sort Characters By Frequency.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
-    //can do it using priority queue instead of vector
+    
+    //OPTIMAL SOLUTION O(N)
+    string frequencySort(string s){
+        unordered_map<char, int> freq;
+        vector<string> v(s.size()+1, "");
+
+        //frequency of each character
+        for(auto c:s){ 
+            freq[c]++;
+        }
+
+        //store chars based on freq as index for vector
+        for(auto it:freq){
+            int i = it.second;
+            char c = it.first;
+            v[i].append(i, c); //appends char c, i times to string v[i]
+        }
+
+        //larger indexes have high freq chars
+        string ans;
+        for(int i=s.size(); i>0; i--){
+            cout<<v[i]<<endl;
+            if(!v[i].empty()){
+                ans.append(v[i]);
+            }
+        }
+
+        return ans;
+    }
+
+
+    //O(NlogN) - solution
     static bool sortby_second(pair<char,int> a,pair<char,int> b){
         return (a.second > b.second); //descending order
     }
@@ -9,7 +40,7 @@ public:
         unordered_map<char, int> m;
         vector<pair<char, int>> v;
 
-        for(auto i:s){ //mapping char to value
+        for(auto i:s){ //mappind char to value
             m[i]++;
         }
 
