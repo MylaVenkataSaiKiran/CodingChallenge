e0ab7f83fd6ff5a245e5f4b477c327953299318f
MylaVenkataSaiKiran <93196950+MylaVenkataSaiKiran@users.noreply.github.com>
2023-11-28 20:54:01 +0530

    sum of diff between maxfreq and min freq of all substrings

---
+++ Strings/Easy/1781. Sum of Beauty of All Substrings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        vector<int> v(26,0); //for using as map
+
+        int maxfreq = INT_MIN, minfreq = INT_MAX, ans = 0;
+
+        for(int i=0; i<s.size(); i++){ //starting char of substr
+            for(int j=i; j<s.size(); j++){ //increasing length of substr
+                v[s[j]-'a']++;
+                maxfreq = *max_element(v.begin(), v.end());
+                for(auto i:v){
+                    if(i > 0){ //it should be > 0, as there is aleast one char
+                        minfreq = min(i, minfreq);
+                    }
+                }
+                ans  += (maxfreq - minfreq);
+                minfreq = INT_MAX; // minfreq may increase in next iteration
+            }
+            fill(v.begin(),v.end(),0); //reset the vector map
+        }
+
+        return ans;
+
+    }
+};
