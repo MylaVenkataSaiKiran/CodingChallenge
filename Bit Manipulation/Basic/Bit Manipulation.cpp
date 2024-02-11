ce6885ee504c78321f5c0b6dbc674b4a334fc661
MylaVenkataSaiKiran <93196950+MylaVenkataSaiKiran@users.noreply.github.com>
2024-02-11 11:44:35 +0530

    Bit manipulation

--- Bit Manipulation/Basic/Bit Manipulation.cpp
+++ Bit Manipulation/Basic/Bit Manipulation.cpp
@@ -1,3 +1,4 @@
+//O(N)-time 
 vector<int> bitManipulation(int num, int i){
     vector<int> ans;
     int temp = num;
@@ -14,5 +15,27 @@ vector<int> bitManipulation(int num, int i){
         ans.push_back(num);
     }
 
+    return ans;
+}
+
+
+//BIT MANIPULATION
+//O(1)-Time
+vector<int> bitManipulation(int num, int i){
+    vector<int>  ans;
+    int bit=0, set=0, clear=0;
+
+    //set 1 at i-th bit and 0s in other places
+    int mask = (1<<(i-1));//will push 1 i-1 times
+
+    if(num & mask){
+        bit=1;
+    }
+
+    set = (num | mask);
+    clear = (num & (~mask));//as mask will have 0s other than i-th bit
+    ans.push_back(bit);
+    ans.push_back(set);
+    ans.push_back(clear);
     return ans;
 }
